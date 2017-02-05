b9fc74852b8996ae4643b499a00988ad14004a53
Giorgi Chighladze <giorgichi@gmail.com>
2017-02-05 12:47:40 -0600

    code for plotting frequency distribution (ecdf)

--- Code/NO3_N_loss_2017_01.R
+++ Code/NO3_N_loss_2017_01.R
@@ -9,145 +9,145 @@ setwd("C:/Users/Gio/Documents")
 load("C:/Users/Gio/Documents/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow.RData")
 
 
-# STJOHNS NO3-N Loss ========================================================================
+# # STJOHNS NO3-N Loss ========================================================================
+# 
+# STJ_NO3_N_conc <- read_excel("~/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/STJOHNS/StJohns_Concentrations_2011-2015.xlsx", 
+#                              col_types = c("date", "text", "text", "numeric"))
+# 
+# names(STJ_NO3_N_conc) <- c("date", "plotid", "grab", "nitrate_conc")
+# STJ_NO3_N_conc %>% 
+#   filter(!is.na(nitrate_conc)) %>%
+#   group_by(plotid, date) %>%
+#   summarize(nitrate_conc = mean(nitrate_conc), grab = first(grab)) %>%
+#   mutate(date = force_tz(ymd_hms(paste(date, "12:00:00")), tzone = "America/New_York")) -> STJ_NO3_N_conc
+# 
+# 
+# #OlsonNames()
+# 
+# 
+# flow %>% 
+#   filter(uniqueid == "STJOHNS") %>% 
+#   arrange(plotid, valid) %>% 
+#   select(uniqueid, plotid, valid, discharge_mm_qc) %>%
+#   mutate(time = with_tz(valid, tzone = "America/New_York")) %>% 
+#   mutate(year = year(time)) %>% 
+#   left_join(STJ_NO3_N_conc, by = c("plotid" = "plotid", "valid" = "date")) -> stj
+# 
+# # replace frist na with the first available nitrate concentration value
+# stj[stj$plotid=="WN", "nitrate_conc"][1] <- stj[stj$plotid=="WN" & !is.na(stj$nitrate_conc), "nitrate_conc"][1]
+# stj[stj$plotid=="WS", "nitrate_conc"][1] <- stj[stj$plotid=="WS" & !is.na(stj$nitrate_conc), "nitrate_conc"][1]
+# 
+# stj <- stj[ , c("uniqueid", "plotid", "year", "time", "discharge_mm_qc", "nitrate_conc", "grab", "valid")]
+# 
+# 
+# stj_wn <- stj[stj$plotid == "WN", ]
+# stj_ws <- stj[stj$plotid == "WS", ]
+#   
+#   
+# zoo(stj_wn$nitrate_conc, stj_wn$valid) %>%
+#   na.approx(na.rm = FALSE) %>%
+#   na.locf() %>%
+#   as.data.frame() -> no3wn
+# no3wn$valid <- ymd_hms(rownames(no3wn))
+# no3wn$plotid <- "WN"
+# rownames(no3wn) <- 1:dim(no3wn)[1]
+# 
+# zoo(stj_ws$nitrate_conc, stj_ws$valid) %>%
+#   na.approx(na.rm = FALSE) %>% 
+#   na.locf() %>% 
+#   as.data.frame() -> no3ws
+# no3ws$valid <- ymd_hms(rownames(no3ws))
+# no3ws$plotid <- "WS"
+# rownames(no3ws) <- 1:dim(no3ws)[1]
+# 
+# rbind(no3wn, no3ws) -> no3
+# names(no3)[1] <- "no3_conc"
+# 
+# stj %>%
+#   left_join(no3, by = c("plotid" = "plotid", "valid" = "valid")) -> stj
+# 
+# #calculate NO3-N loss in kg/ha
+# stj %>% 
+#   mutate(area = ifelse(plotid == "WN", 7.16, 7.28)) %>%
+#   mutate(no3_loss = discharge_mm_qc * 10000 * no3_conc / 10^6) %>%            #to convert to gallons (* 0.264172 * area)
+#   group_by(plotid, year) %>%
+#   summarise(loss = sum(no3_loss, na.rm = TRUE)) -> stj_loss
+#  
+# 
+# 
+# # DPAC NO3-N Loss ===========================================================
+# DPAC_NO3_N_conc <- read_excel("~/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/DPAC/DPAC Water Quality Data 2017.xlsx", 
+#                              col_types = c("text", "date", "text", "numeric", "numeric", "numeric"))
+# 
+# names(DPAC_NO3_N_conc) <- c ("plotid", "date", "grab", "nitrate_conc", "ortho_P", "total_P")
+# DPAC_NO3_N_conc %>% 
+#   select(1:4) %>%
+#   filter(!is.na(nitrate_conc)) %>%
+#   group_by(plotid, date) %>%
+#   summarize(nitrate_conc = mean(nitrate_conc), grab = first(grab)) %>%
+#   mutate(date = force_tz(ymd_hms(paste(date, "12:00:00")), tzone = "America/New_York")) -> DPAC_NO3_N_conc
+# 
+# 
+# #OlsonNames()
+# 
+# 
+# flow %>% 
+#   filter(uniqueid == "DPAC") %>% 
+#   arrange(plotid, valid) %>%
+#   select(uniqueid, plotid, valid, discharge_mm_qc) %>%
+#   mutate(time = with_tz(valid, tzone = "America/New_York")) %>%
+#   mutate(year = year(time)) %>% 
+#   left_join(DPAC_NO3_N_conc, by = c("plotid" = "plotid", "valid" = "date")) -> dpac
+# 
+# 
+# 
+# # extract no3-n conc data that was measured before flow measurement start 
+# # (i.e. the last 2011 no3-n data to be used for interpolation from 2012)
+# DPAC_NO3_N_conc %>% 
+#   mutate(year = year(date)) %>%
+#   filter(year == 2011) %>%
+#   group_by(plotid) %>%
+#   filter(date == max(date)) -> dpac_no3_n_2011
+# names(dpac_no3_n_2011)[which(names(dpac_no3_n_2011) == "date")] <- "time"
+#  
+# dpac %>%
+#   bind_rows(dpac_no3_n_2011) %>%
+#   arrange(plotid, time) -> dpac
+# 
+# 
+# dpac <- dpac[ , c("uniqueid", "plotid", "year", "time", "discharge_mm_qc", "nitrate_conc", "grab", "valid")]
+# 
+# dpac %>%
+#   arrange(plotid, desc(time)) %>%
+#   group_by(plotid) %>%
+#   mutate(new_grab = na.locf(grab, na.rm = F)) %>%
+#   mutate(no3_conc = ifelse(new_grab == "C", na.locf(nitrate_conc, na.rm = FALSE), nitrate_conc)) %>%
+#   arrange(plotid, time) %>%
+#   mutate(no3_conc = na.approx(no3_conc, na.rm = FALSE)) %>%
+#   mutate(no3_conc = na.locf(no3_conc, na.rm = FALSE)) -> dpac
+#   
+# 
+# #calculate NO3-N loss in kg/ha
+# dpac %>% 
+#   mutate(no3_loss = discharge_mm_qc * 10000 * no3_conc / 10^6) %>%            #to convert to gallons (* 0.264172 * area)
+#   group_by(plotid, year) %>%
+#   summarise(loss = sum(no3_loss, na.rm = TRUE)) %>%
+#   filter(year != 2011) -> dpac_loss
+# 
+# 
+# 
+# 
 
-STJ_NO3_N_conc <- read_excel("~/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/STJOHNS/StJohns_Concentrations_2011-2015.xlsx", 
-                             col_types = c("date", "text", "text", "numeric"))
-
-names(STJ_NO3_N_conc) <- c("date", "plotid", "grab", "nitrate_conc")
-STJ_NO3_N_conc %>% 
-  filter(!is.na(nitrate_conc)) %>%
-  group_by(plotid, date) %>%
-  summarize(nitrate_conc = mean(nitrate_conc), grab = first(grab)) %>%
-  mutate(date = force_tz(ymd_hms(paste(date, "12:00:00")), tzone = "America/New_York")) -> STJ_NO3_N_conc
-
-
-#OlsonNames()
-
-
-flow %>% 
-  filter(uniqueid == "STJOHNS") %>% 
-  arrange(plotid, valid) %>% 
-  select(uniqueid, plotid, valid, discharge_mm_qc) %>%
-  mutate(time = with_tz(valid, tzone = "America/New_York")) %>% 
-  mutate(year = year(time)) %>% 
-  left_join(STJ_NO3_N_conc, by = c("plotid" = "plotid", "valid" = "date")) -> stj
-
-# replace frist na with the first available nitrate concentration value
-stj[stj$plotid=="WN", "nitrate_conc"][1] <- stj[stj$plotid=="WN" & !is.na(stj$nitrate_conc), "nitrate_conc"][1]
-stj[stj$plotid=="WS", "nitrate_conc"][1] <- stj[stj$plotid=="WS" & !is.na(stj$nitrate_conc), "nitrate_conc"][1]
-
-stj <- stj[ , c("uniqueid", "plotid", "year", "time", "discharge_mm_qc", "nitrate_conc", "grab", "valid")]
-
-
-stj_wn <- stj[stj$plotid == "WN", ]
-stj_ws <- stj[stj$plotid == "WS", ]
-  
-  
-zoo(stj_wn$nitrate_conc, stj_wn$valid) %>%
-  na.approx(na.rm = FALSE) %>%
-  na.locf() %>%
-  as.data.frame() -> no3wn
-no3wn$valid <- ymd_hms(rownames(no3wn))
-no3wn$plotid <- "WN"
-rownames(no3wn) <- 1:dim(no3wn)[1]
-
-zoo(stj_ws$nitrate_conc, stj_ws$valid) %>%
-  na.approx(na.rm = FALSE) %>% 
-  na.locf() %>% 
-  as.data.frame() -> no3ws
-no3ws$valid <- ymd_hms(rownames(no3ws))
-no3ws$plotid <- "WS"
-rownames(no3ws) <- 1:dim(no3ws)[1]
-
-rbind(no3wn, no3ws) -> no3
-names(no3)[1] <- "no3_conc"
-
-stj %>%
-  left_join(no3, by = c("plotid" = "plotid", "valid" = "valid")) -> stj
-
-#calculate NO3-N loss in kg/ha
-stj %>% 
-  mutate(area = ifelse(plotid == "WN", 7.16, 7.28)) %>%
-  mutate(no3_loss = discharge_mm_qc * 10000 * no3_conc / 10^6) %>%            #to convert to gallons (* 0.264172 * area)
-  group_by(plotid, year) %>%
-  summarise(loss = sum(no3_loss, na.rm = TRUE)) -> stj_loss
- 
-
-
-# DPAC NO3-N Loss ===========================================================
-DPAC_NO3_N_conc <- read_excel("~/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/DPAC/DPAC Water Quality Data 2017.xlsx", 
-                             col_types = c("text", "date", "text", "numeric", "numeric", "numeric"))
-
-names(DPAC_NO3_N_conc) <- c ("plotid", "date", "grab", "nitrate_conc", "ortho_P", "total_P")
-DPAC_NO3_N_conc %>% 
-  select(1:4) %>%
-  filter(!is.na(nitrate_conc)) %>%
-  group_by(plotid, date) %>%
-  summarize(nitrate_conc = mean(nitrate_conc), grab = first(grab)) %>%
-  mutate(date = force_tz(ymd_hms(paste(date, "12:00:00")), tzone = "America/New_York")) -> DPAC_NO3_N_conc
-
-
-#OlsonNames()
-
-
-flow %>% 
-  filter(uniqueid == "DPAC") %>% 
-  arrange(plotid, valid) %>%
-  select(uniqueid, plotid, valid, discharge_mm_qc) %>%
-  mutate(time = with_tz(valid, tzone = "America/New_York")) %>%
-  mutate(year = year(time)) %>% 
-  left_join(DPAC_NO3_N_conc, by = c("plotid" = "plotid", "valid" = "date")) -> dpac
-
-
-
-# extract no3-n conc data that was measured before flow measurement start 
-# (i.e. the last 2011 no3-n data to be used for interpolation from 2012)
-DPAC_NO3_N_conc %>% 
-  mutate(year = year(date)) %>%
-  filter(year == 2011) %>%
-  group_by(plotid) %>%
-  filter(date == max(date)) -> dpac_no3_n_2011
-names(dpac_no3_n_2011)[which(names(dpac_no3_n_2011) == "date")] <- "time"
- 
-dpac %>%
-  bind_rows(dpac_no3_n_2011) %>%
-  arrange(plotid, time) -> dpac
-
-
-dpac <- dpac[ , c("uniqueid", "plotid", "year", "time", "discharge_mm_qc", "nitrate_conc", "grab", "valid")]
-
-dpac %>%
-  arrange(plotid, desc(time)) %>%
-  group_by(plotid) %>%
-  mutate(new_grab = na.locf(grab, na.rm = F)) %>%
-  mutate(no3_conc = ifelse(new_grab == "C", na.locf(nitrate_conc, na.rm = FALSE), nitrate_conc)) %>%
-  arrange(plotid, time) %>%
-  mutate(no3_conc = na.approx(no3_conc, na.rm = FALSE)) %>%
-  mutate(no3_conc = na.locf(no3_conc, na.rm = FALSE)) -> dpac
-  
-
-#calculate NO3-N loss in kg/ha
-dpac %>% 
-  mutate(no3_loss = discharge_mm_qc * 10000 * no3_conc / 10^6) %>%            #to convert to gallons (* 0.264172 * area)
-  group_by(plotid, year) %>%
-  summarise(loss = sum(no3_loss, na.rm = TRUE)) %>%
-  filter(year != 2011) -> dpac_loss
-
-
-
-
-
-# MANAGE db
-manage_drain <- 
+# MANAGE db =====================================================
+manage_drain <-
   read_excel("~/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/MANAGE DB/manage2016/drain_load_short_10.2015.xlsx")
 
-manage_drain %>% 
-  select(RefID, Year, `Watershed ID`, State, 
+manage_drain %>%
+  select(RefID, Year, `Watershed ID`, State,
          `Drainage type`, `Drain depth (m)`, `Drain spacing (m)`,
-         `Land Use`, Tillage, `Dominant Soil Type`, 
+         `Land Use`, Tillage, `Dominant Soil Type`,
          `Crop 1`, `Avg Crop 1 Yield (Mg/ha)`, `Crop 2`, `Avg Crop 2 Yield (Mg/ha)`,
-         `Avg Precipitation (mm)`, `Drainage Discharge (mm)`, 
+         `Avg Precipitation (mm)`, `Drainage Discharge (mm)`,
          `Avg Soil Loss (kg/ha)`, `Avg Dissolved N (kg/ha)`, `Avg Dissolved P (kg/ha)`, `Avg Total P (kg/ha)`,
          Comments) %>%
   filter(!is.na(`Avg Dissolved N (kg/ha)`)) %>%                # select entries with NO3-N load data
@@ -231,6 +231,7 @@ ggsave(filename = "no3n_01.png", width = 12, height = 8, dpi = 300)
 NO3_N_Loss %>%
   filter(complete.cases(.)) %>%
   filter(SITE != "WATERMAN") %>%
+  #filter(YEAR > 2010) %>%
   filter(Treatment != "shallow drainage") %>%
   group_by(Treatment) %>%
   mutate(number = n()) %>%
@@ -384,6 +385,62 @@ ggsave(filename = "no3n_05_l.png", width = 12, height = 8, dpi = 300)
 
 
 
+
+NO3_N_Loss %>%
+  filter(complete.cases(.)) %>%
+  filter(YEAR > 2010) %>%
+  filter(SITE != "WATERMAN") %>%
+  filter(Treatment != "shallow drainage") %>%
+  group_by(Treatment, SITE) %>%
+  mutate(number = n()) %>%
+  mutate(se = sd(`NO3-N Loss (kg/ha)`)/sqrt(number)) %>%
+  mutate(mean = mean(`NO3-N Loss (kg/ha)`)) %>%
+  ggplot(aes(x = SITE, y = `NO3-N Loss (kg/ha)`, fill = Treatment)) +
+  geom_bar(stat = "summary", fun.y = "mean", position = "dodge") + 
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width = 0.3, size = 1, position = position_dodge(.9)) +
+  geom_text(aes(y = 45, label = number, colour = I("grey60")), vjust = 1, size = 7) +
+  theme_light() +
+  xlab("") +
+  ggtitle("Annual Nitrate-N Load by Site") +
+  theme(plot.title = element_text(hjust = 0.5, vjust = 0.5, size = 20, face = "bold"),
+        axis.title = element_text(face = "bold", colour = "grey30", size = 16),
+        axis.text.x = element_text(size = 12, vjust = 0.5, face = "bold"),
+        axis.text.y = element_text(size = 12, vjust = 0.5)) +
+  scale_fill_brewer() +
+  coord_cartesian(ylim = c(0, 45))
+ggsave(filename = "no3n_06_5years.png", width = 12, height = 8, dpi = 300)
+
+
+
+NO3_N_Loss %>%
+  filter(complete.cases(.)) %>%
+  filter(YEAR > 2010) %>%
+  filter(SITE != "WATERMAN") %>%
+  filter(SITE != "GILMORE") %>%
+  filter(Treatment != "shallow drainage") %>%
+  group_by(Treatment, YEAR) %>%
+  mutate(number = n()) %>%
+  mutate(se = sd(`NO3-N Loss (kg/ha)`)/sqrt(number)) %>%
+  mutate(mean = mean(`NO3-N Loss (kg/ha)`)) %>%
+  ggplot(aes(x = Treatment, y = `NO3-N Loss (kg/ha)`, fill = Treatment)) +
+  geom_bar(stat = "summary", fun.y = "mean", position = "stack") + 
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width = 0.3, size = 1) +
+  facet_grid( ~ YEAR) +
+  #geom_text(aes(y = 50, label = number, colour = I("grey60")), vjust = 1, size = 7) +
+  scale_x_discrete(name ="") +
+  theme_light() +
+  labs(title = "Annual Nitrate-N Loss", subtitle = "without GILMORE data") +
+  theme(plot.title = element_text(hjust = 0.5, vjust = 0.5, size = 20, face = "bold"),
+        plot.subtitle = element_text(hjust = 0.5, size = 16),
+        axis.title = element_text(face = "bold", colour = "grey30", size = 16),
+        axis.text.y = element_text(size = 12, vjust = 0.5),
+        axis.text.x = element_blank()) +
+  scale_fill_brewer() +
+  coord_cartesian(ylim = c(0, 50))
+ggsave(filename = "no3n_07_5years.png", width = 12, height = 8, dpi = 300)
+
+
+
 # MERGE TWO DATA ==============================================================
 manage_NO3_N %>%
   select(state, siteID, plotID, 
@@ -403,10 +460,12 @@ bind_rows(cscap, manage) %>%
          drainage_type = as.factor(drainage_type),
          project = as.factor(project)) -> mcap
 
-save(mcap, file = "~/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/cap_manage.Rda")
-  
+#save(mcap, file = "~/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/cap_manage.Rda")
+#load("~/GitHub/CSCAP/Sustainable_Corn_Paper/Data/flow/cap_manage.Rda")  
 
 mcap %>%
+  filter(state %in% c("IA", "MO", "MN", "IN", "OH", "IL")) %>%
+  filter(drainage_type %in% c("free drainage", "cont. drainage", "Subsurface (no inlets specified)")) %>%
   ggplot(aes(x=drainage_type, y=N_load, fill = project)) +
   geom_boxplot(na.rm = TRUE) +
   stat_summary(fun.y = "mean", geom = "point", shape = 3, stroke = 1.5, color = "black", size = 3) +
@@ -428,18 +487,88 @@ mcap %>%
   mutate(number = n()) %>%
   mutate(mean = mean(N_load),
          se = sd(N_load)/sqrt(number)) %>%
+  filter(state %in% c("IA", "MO", "MN", "IN", "OH", "IL")) %>%
+  filter(drainage_type %in% c("free drainage", "cont. drainage", "Subsurface (no inlets specified)")) %>%
+  filter((project == "CSCAP" & year > 2010) | (project == "MANAGE")) %>%
   ggplot(aes(x=year, y=N_load, fill = project)) +
-  geom_bar(stat = "summary", fun.y = "mean", position = "stack") + 
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width = 0.3) +
-  facet_grid(drainage_type ~ .) +
-  scale_x_discrete(name ="") +
+  geom_bar(stat = "summary", fun.y = "mean", position = "stack", na.rm = T) + 
+  facet_grid(drainage_type ~ ., labeller = label_wrap_gen(width = 20)) +
   theme_light() +
-  ggtitle("Annual Nitrate-N Load") +
+  ggtitle("Average Annual NO3-N Load") +
+  ylab("Nitrate-N Load, kg/ha") +
+  xlab("") +
   theme(plot.title = element_text(hjust = 0.5, vjust = 0.5, size = 20, face = "bold"),
         axis.title = element_text(face = "bold", colour = "grey30", size = 16),
-        axis.text.y = element_text(size = 12, vjust = 0.5),
-        axis.text.x = element_blank(),
-        strip.text = element_text(size = 6, face ="bold")) +
-  scale_fill_brewer() +
-  coord_cartesian(ylim = c(0, 80))
+        axis.text = element_text(size = 12, vjust = 0.5),
+        strip.text = element_text(size = 14, face ="bold")) +
+  scale_fill_brewer(palette = "Accent") +
+  coord_cartesian(ylim = c(0, 70))
 ggsave(filename = "no3n_mcap_2.png", width = 12, height = 8, dpi = 300)
+
+
+mcap %>% 
+  filter(state %in% c("IA", "MO", "MN", "IN", "OH", "IL")) %>%
+  filter(drainage_type %in% c("free drainage", "cont. drainage", "Subsurface (no inlets specified)")) %>%
+  ggplot(aes(x=N_load, 
+             group=interaction(project,drainage_type), 
+             color = interaction(project,drainage_type))) + 
+  stat_ecdf(na.rm = T, lwd = 1, geom = "line") +
+  theme_light() +
+  scale_color_brewer(palette = "Accent", labels = c("Controlled Drainage - CSCAP", 
+                                                    "Free Drainage - CSCAP", 
+                                                    "Free Drainage - MANAGE")) +
+  xlab("Nitrate-N Load, kg/ha") +
+  ylab("Cummulative Density") +
+  labs(color = "", title = "Nitrate-N Load from Ag Fields in Corn Belt Region") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
+        axis.title = element_text(size = 14),
+        axis.text  = element_text(size = 12),
+        legend.text = element_text(size = 12),
+        legend.position = c(0.8, 0.2))
+
+ggsave(filename = "no3n_mcap_3.png", width = 10, height = 7, dpi = 300)
+
+
+library(splines)
+library(dplyr)
+
+mcap %>% 
+  filter(state %in% c("IA", "MO", "MN", "IN", "OH", "IL")) %>%
+  filter(drainage_type %in% c("free drainage", "cont. drainage", "Subsurface (no inlets specified)")) %>%
+  group_by(interaction(project,drainage_type)) %>%
+  mutate(ecd = ecdf(N_load)(N_load)) %>%
+  ungroup() %>%
+  ggplot(aes(x = N_load, y = ecd, group = drainage_type, colour = drainage_type)) +
+  geom_smooth(se = FALSE, formula = y ~ ns(x, 15), method = "lm", lwd = 1.5) +
+  #geom_line(lwd = 1.5) +
+  theme_light() +
+  scale_color_brewer(palette = "Accent", labels = c("Controlled Drainage - CSCAP", 
+                                                    "Free Drainage - CSCAP", 
+                                                    "Free Drainage - MANAGE")) +
+  xlab("Nitrate-N Load, kg/ha") +
+  ylab("Cummulative Density") +
+  labs(color = "", title = "Nitrate-N Load from Ag Fields in Corn Belt Region") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
+        axis.title = element_text(size = 14),
+        axis.text  = element_text(size = 12),
+        legend.text = element_text(size = 12),
+        legend.position = c(0.8, 0.2)) -> smooth_mcap
+smooth_mcap
+ggsave(filename = "no3n_mcap_3_smooth.png", width = 10, height = 7, dpi = 300)
+
+
+vlines <- data.frame(regions = c("Iowa", "Indiana", "Minnesota"),
+                     max_loads = c(10, 15, 25), 
+                     y_value = rep(0, 3))
+
+smooth_mcap +
+  scale_x_continuous(breaks = sort(c(0, 50, 100, 150, vlines$max_loads))) +
+  geom_vline(xintercept = vlines$max_loads, size = 1, colour = "gray40", alpha = 0.5, 
+             linetype = "longdash") +
+  theme(panel.grid.minor.x = element_blank()) +
+  geom_text(inherit.aes = F, data=vlines, mapping = aes(x=max_loads, y=y_value, label = regions),
+            size = 4, fontface = "bold", color = "gray40", angle = 90, hjust = 0, vjust = -0.2)
+ 
+
+
+
